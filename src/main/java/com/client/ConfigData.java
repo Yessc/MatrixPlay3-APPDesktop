b84988c21004d60fecc41adddabb2722905026c1
Yessc <dianabailon06@gmail.com>
2025-11-12 19:14:38 +0100

    revisado

--- src/main/java/com/client/ConfigData.java
+++ src/main/java/com/client/ConfigData.java
@@ -7,8 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ConfigData {//acordarme de mirar
-
+public class ConfigData {
     private String playerName;
     private String clientType;
     private List<Integer> scores; // lista de puntuaciones futura
@@ -24,7 +23,6 @@ public class ConfigData {//acordarme de mirar
         this.posY = 0.0;
     }
  
-    // Getters y setters
     public String getPlayerName() {
         return playerName;
     }
@@ -61,8 +59,8 @@ public class ConfigData {//acordarme de mirar
                 if (i < scores.size() - 1)
                     writer.write(",");
             }
-            writer.write("]\n");
-            writer.write("}");*/// lo guardamos para despues
+            writer.write("]\n");*/// lo guardamos para despues
+            writer.write("}");
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/client/ConfigWaiting.java
+++ src/main/java/com/client/ConfigWaiting.java
@@ -4,13 +4,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.application.Platform;
 
 
 public class ConfigWaiting {
 
     @FXML
-    private ImageView player1Image;
+    private ImageView player1Image;//agregar imagenes
 
     @FXML
     private ImageView player2Image;
@@ -98,7 +97,7 @@ public class ConfigWaiting {
 
             System.out.println("¡Two players Ready!");
 
-            // Crear un nuevo hilo para esperar un segundo
+           /* 
             Thread waitThread = new Thread(() -> {
                 try {
                     // Simula la espera de 1 segundo (1000 milisegundos)
@@ -126,10 +125,10 @@ public class ConfigWaiting {
                 });
             });
 
-        // Iniciar el hilo
-        waitThread.start();
+        
+        waitThread.start();*/
+        }
     }
-}
 
     
     
